7fa7e837c9334f50604f0e425d26a3b5b903569d
khaledahmed0918-sys <khaledahmed0918@gmail.com>
2025-11-12 12:43:57 +0300

    refactor: Remove service worker registration
    The service worker registration code in `index.tsx` and the service worker files themselves have been removed. This simplifies the application by removing unused PWA functionalities.

--- index.tsx
+++ index.tsx
@@ -1,14 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LocalizationProvider } from './hooks/useLocalization';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch(console.error);
-  });
-}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
